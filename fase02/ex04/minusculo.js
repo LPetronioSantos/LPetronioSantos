2a9ef2beecda111517cd9776e56905d48cafde76
LPetronioSantos <57539071+LPetronioSantos@users.noreply.github.com>
2019-11-17 20:58:01 -0300

    Prim-Minuscula

--- fase02/ex04/minusculo.js
+++ fase02/ex04/minusculo.js
@@ -1,5 +1,5 @@
 function minusculo(min){
     priM = min.substring(0,1);
-    restante = min.substring(1)
-    return  priM.toLowerCase()+ restante;
+    return  priM.toLowerCase();
     }
+    console.log(minusculo("Lucas"));
