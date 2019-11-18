68ba5cd7ca7ea8e74a64c7a613c7cb23a77bf50e
LPetronioSantos <57539071+LPetronioSantos@users.noreply.github.com>
2019-11-18 15:14:13 -0300

    vogalouconsoante

---
+++ fase03/ex03/vogalOuConsoante.js
@@ -0,0 +1,7 @@
+ function vogalOuConsoante(letra){
+    letram = letra.toLowerCase();
+    if (letram == "a" || letra == "e"||letra == "i" || letra == "o" || letra == "u" )
+    {return("Vogal")
+    ;}else{return("Consoante");
+    }
+}
