a7fdeedda313687847c118380ecfbc2b69a9f8b9
LPetronioSantos <57539071+LPetronioSantos@users.noreply.github.com>
2019-11-18 15:51:08 -0300

    acharNumero

---
+++ fase04/ex05/acharNumero.js
@@ -0,0 +1,8 @@
+function acharNumero(num){
+    if (num < 5){return "mercurio"}
+         if( num >= 5 & num <= 10){return "venus"
+         ;}
+            if( num > 10 & num <= 100){return "terra"
+         ;}
+             else {return "marte"}
+}
