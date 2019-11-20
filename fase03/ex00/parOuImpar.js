65001e3c81ab456ac22416f7bbf875507086327a
LPetronioSantos <57539071+LPetronioSantos@users.noreply.github.com>
2019-11-20 16:16:49 -0300

    Datas

--- fase03/ex00/parOuImpar.js
+++ fase03/ex00/parOuImpar.js
@@ -1,5 +1,26 @@
- function parOuImpar(num){
-    if (num%2 === 0){return("Par")
-    ;}else{return("Ímpar")}
-     
+function tempo(data1, data2, unidade){
+    d1 = new Date(data1);
+    d2 = new Date(data2);
+    a1 = d1.getUTCFullYear();
+    a2 = d2.getUTCFullYear();
+    dia1 = d1.getUTCDay();
+    dia2 = d2.getUTCDay();
+
+    
+    if (unidade == "ano"){return Math.abs(a1-a2)
+    }
+        if( unidade == "dia"){return (dia1 - dia2)
+         ;}
+            if(unidade == "mês"){return
+         ;}
 }
+
+console.log(tempo(1243817066000, "2009-07-28T19:24:00", "dia"));
+/*var d1 = ("2018-01-01")
+var d2 = ("2019-03-22")
+var v1 = new Date(d1)
+var v2 = new Date(d2)
+var r1 = v1.getUTCFullYear();
+var r2 = v2.getUTCFullYear();
+result = Math.abs(parseInt(r1-r2))
+console.log(result);*/
